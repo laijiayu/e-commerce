3e0abeb273971358cf384ea392c554dd5aaaaa39
Allison <theallisonlai@fakemail.com>
2023-08-01 01:33:15 +0800

    fix-bug

--- functions/products.js
+++ functions/products.js
@@ -1,8 +1,7 @@
 const dotenv = require("dotenv")
 dotenv.config()
 
-const Airtable = require("airtable-node")
-
+let Airtable = require("airtable-node")
 const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
   .base(process.env.AIRTABLE_BASE)
   .table(process.env.AIRTABLE_TABLE)
